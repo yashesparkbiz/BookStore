733872f3f3e3a78aa7a78af443d24e448ebe2f2a
Yash M <Yash M@DESKTOP-0KNEP33>
2022-04-13 19:24:38 +0530

    book store web app created

---
+++ BookStore1/Program.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore1
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            CreateHostBuilder(args).Build().Run();
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
+            {
+                webBuilder.UseStartup<Startup>();
+            });
+    }
+}
+
+//var builder = WebApplication.CreateBuilder(args);
+
+//// Add services to the container.
+//builder.Services.AddRazorPages();
+
+//var app = builder.Build();
+
+//// Configure the HTTP request pipeline.
+//if (!app.Environment.IsDevelopment())
+//{
+
+//    app.UseExceptionHandler("/Error");
+//    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//    app.UseHsts();
+//}
+
+//app.Use(async (context, next) =>
+//{
+//    // Do work that can write to the Response.
+//    await next.Invoke();
+//    // Do logging or other work that doesn't write to the Response.
+//});
+
+//app.Run(async context =>
+//{
+//    await context.Response.WriteAsync("Hello from 2nd delegate.");
+//});
+
+
+//app.UseHttpsRedirection();
+//app.UseStaticFiles();
+
+//app.UseRouting();
+
+//app.UseAuthorization();
+
+//app.MapRazorPages();
+
+//app.Run();
---
+++ BookStore1/Startup.cs
@@ -0,0 +1,56 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace BookStore1
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseRouting();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World ");
+                });
+            });
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.Map("/yash", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World from yash");
+                });
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from my first middleware  \n");
+                await next();
+            });
+
+            app.Use(async (context, next) =>
+            {
+                await context.Response.WriteAsync("Hello from my Second middleware  \n");
+                await next();
+            });
+
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello from 2nd delegate.  \n");
+            });
+
+            
+        }
+    }
+}
