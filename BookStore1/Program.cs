16d9a92ef5c0f3f6ad8f664c2cb031fa27c75fa0
Yash M <Yash M@DESKTOP-0KNEP33>
2022-04-14 15:51:31 +0530

    sdas

--- BookStore1/Program.cs
+++ BookStore1/Program.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
-var builder = WebApplication.CreateBuilder(args);
+
 
 namespace BookStore1
 {
